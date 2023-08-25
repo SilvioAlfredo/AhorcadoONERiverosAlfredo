9699d7ff66d008495a4883cb143fd141b6df1077
Alfredo Riveros <silvio.riveros.94@gmail.com>
2023-08-24 22:45:40 -0300

    modificación de css y estructura de archivos.

--- js/medio.js
+++ js/medio.js
@@ -1,33 +1,33 @@
-var botonCancelar= document.querySelector(".cancelar");
+var botonCancelar = document.querySelector(".cancelar");
 
-botonCancelar.addEventListener("click",function(event){
-	event.preventDefault();
-    window.location.href = "./index.html";
+botonCancelar.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "./index.html";
 });
 
 var botonGuardar = document.querySelector(".guardarStart");
 var entrada = document.querySelector(".entrada");
 var expreg = new RegExp(/[a-zA-Z]+/g);
 
-botonGuardar.addEventListener("click", function(event){
-    event.preventDefault();
-    validarCadena();
+botonGuardar.addEventListener("click", function (event) {
+  event.preventDefault();
+  validarCadena();
 });
 
-function validarCadena(){
-    let texto = entrada.value;
-    text = texto.toUpperCase();	
-    if (text.length > 8){
-        alert("El texto excede el tamaño requerido!");
-        entrada.focus();
-    }else if(entrada.value == ""){
-        alert("campo incorrecto!");
-        entrada.focus();
-    }else if(expreg.test(text)){
-            sessionStorage.setItem("text", document.querySelector(".entrada").value);
-            window.location.href = "./horca.html";
-    }else if(!expreg.test(text)){
-        alert("expresión incorrecta!");
-        entrada.focus();
-    }
+function validarCadena() {
+  let texto = entrada.value;
+  text = texto.toUpperCase();
+  if (text.length > 8) {
+    alert("El texto excede el tamaño requerido!");
+    entrada.focus();
+  } else if (entrada.value == "") {
+    alert("campo incorrecto!");
+    entrada.focus();
+  } else if (expreg.test(text)) {
+    sessionStorage.setItem("text", document.querySelector(".entrada").value);
+    window.location.href = "./horca.html";
+  } else if (!expreg.test(text)) {
+    alert("expresión incorrecta!");
+    entrada.focus();
+  }
 }
